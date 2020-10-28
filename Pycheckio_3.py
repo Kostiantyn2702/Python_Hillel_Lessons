9d785aecf019e3e7020653bd242851fe591d5a8b
Kostiantyn2702 <Kostiantyn2702@users.noreply.github.com>
2020-10-28 15:40:32 +0200

    Обновление

--- Pycheckio_3.py
+++ Pycheckio_3.py
@@ -76,7 +76,8 @@
 
 """
 Дано положительное целое число. Вам необходимо подсчитать произведение всех цифр в этом числе, за исключением нулей.
-Для примера: Дано число 123405. Результат будет: 1*2*3*4*5=120 (не забудьте исключить нули)."""
+Для примера: Дано число 123405. Результат будет: 1*2*3*4*5=120 (не забудьте исключить нули).
+"""
 
 # def checkio(number: int) -> int:
 #     answer = 1
@@ -89,32 +90,48 @@
 In this mission you need to create a password verification function.
 Those are the verification conditions:
 the length should be bigger than 6;
-should contain at least one digit.
+should contain at least one digit, but it cannot consist of just digits;
+having numbers or containing just numbers does not apply to the password longer than 9.
+a string should not contain the word "password" in any case;
+should contain 3 different letters (or digits) even if it is longer than 10
 """
 
 # def is_acceptable_password(password: str) -> bool:
 #     is_digit = "1234567890"
-#     if len(password) < 6 or len(password) == 6:
+#     if len(password) <= 6:
 #         return False
-#     for digits in is_digit:
-#         if digits in password:
-#             return True
-#     return False
-
-"""
-In this mission you need to create a password verification function.
-Those are the verification conditions:
-the length should be bigger than 6;
-should contain at least one digit, but cannot consist of just digits
-"""
-
-# def is_acceptable_password(password: str) -> bool:
+#     if "password" in password or "PASSWORD" in password:
+#         return False
+#     if len(set(password)) < 3:
+#         return False
+#     if len(password) > 9:
+#         return True
 #     if password.isdigit():
 #         return False
-#     is_digit = "1234567890"
-#     if len(password) < 6 or len(password) == 6:
-#         return False
 #     for digits in is_digit:
 #         if digits in password:
 #             return True
 #     return False
+
+"""Check if a given string has all symbols in upper case. If the string is empty or doesn't have any letter in it - function should return False."""
+
+# def is_all_upper(text: str) -> bool:
+#     return True if text.isupper() else False
+
+"""Determine whether the sequence of elements items is ascending so that its each element is strictly larger than (and not merely equal to) the element that precedes it."""
+
+# def is_ascending(items) -> bool:
+#     empty_list = [-1000000]
+#     index = 0
+#     if len(set(items)) == 1 and len(items) > 1:
+#         return False
+#     for item in items:
+#         if item < empty_list[index]:
+#             return False
+#         index += 1
+#         empty_list.append(item)
+#     return True
+
+# def is_ascending(items) -> bool:
+#     return all(items[i] < items[i+1] for i in range(len(items)-1))
+
